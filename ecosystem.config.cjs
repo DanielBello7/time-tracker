dc69db2a8b92092ba59ac9ee1c583796c494f091
DanielBello7 <gokebello@gmail.com>
2024-03-08 09:11:38 -0500

    chore: updates

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: 'activity-tracker',
+      name: 'core-task',
       exec_mode: 'cluster',
       instances: 1,
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       env_local: {
-        APP_ENV: 'local'
-      },
-      env_development: {
-        APP_ENV: 'dev'
+        APP_ENV: '.env.development'
       },
       env_production: {
-        APP_ENV: 'prod'
+        APP_ENV: '.env.production'
       }
     }
   ]
