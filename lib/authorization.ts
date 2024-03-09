7e35850e7276566a6ed7a2b9134c8a8d4f34ead4
DanielBello7 <gokebello@gmail.com>
2024-03-09 11:35:53 -0500

    chore: updates

--- lib/authorization.ts
+++ lib/authorization.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { NextHandler } from "next-connect";
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authenticationOptions } from "@/pages/api/auth/[...nextauth]";
 import BaseError from "./base-error";
 
 export default async function authorization(
   req: NextApiRequest, res: NextApiResponse, next: NextHandler
 ) {
-  const session = await getSession({ req });
+  const session = await getServerSession(req, res, authenticationOptions);
   if (session) return await next();
   throw new BaseError(401, "unauthorized");
 }
--- lib/dual-authorization.ts
+++ lib/dual-authorization.ts
@@ -1,16 +1,19 @@
+import { authenticationOptions } from "@/pages/api/auth/[...nextauth]";
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { NextHandler } from "next-connect";
-import { getSession } from "next-auth/react";
-import BaseError from "./base-error";
 import validateJwt from "./validate-jwt";
+import BaseError from "./base-error";
+import { getServerSession } from "next-auth";
 
 export default async function dualAuthorization(
-  req: NextApiRequest, _: NextApiResponse, next: NextHandler
+  req: NextApiRequest, res: NextApiResponse, next: NextHandler
 ) {
   const auth_header = req.headers['authorization'];
   const token = auth_header ? auth_header.split(' ')[1] : "";
+
   const response = validateJwt(token);
-  const session = await getSession({ req });
+  const session = await getServerSession(req, res, authenticationOptions);
+
   if (session || response !== false) return await next();
   throw new BaseError(401, "Unauthorized");
 }
